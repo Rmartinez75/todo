d06f4fcad8ad329a7a23c567eb2dadbc4a91f557
Rmartinez75 <rmartinez75@gmail.com>
2020-08-28 11:31:23 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template class="md-layout">
+  <div class="md-layout-item">
+    <md-field>
+      <md-input
+        class="center-content"
+        v-model="currentTodo"
+        @keydown.enter="addTodo()"
+        placeholder="Add a todo"
+      />
+    </md-field>
+    <div class="center-content">
+      <span>Completed ToDo Delete</span>
+    </div>
+    <div class="md-layout-item">
+      <ul class="center-content todos">
+        <li v-for="todo in todos" :key="todo.id">
+          <input type="checkbox" />
+          {{ todo.label }}
+        </li>
+      </ul>
+    </div>
+    <div class="md-layout-item"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].editing = !this.todos[index].editing;
+    },
+  },
+};
+</script>
+
+<style>
+.center-content {
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+}
+ul {
+  list-style-type: none;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import App from './App.vue';
+import { MdField } from 'vue-material/dist/components';
+import { MdCard } from 'vue-material/dist/components';
+import { MdCheckbox } from 'vue-material/dist/components';
+import { MdSwitch } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default-dark.css';
+
+Vue.use(VueMaterial);
+Vue.use(MdField);
+Vue.use(MdCard);
+Vue.use(MdCheckbox);
+Vue.use(MdSwitch);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
